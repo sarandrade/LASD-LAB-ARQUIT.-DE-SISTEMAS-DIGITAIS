381a1ae008c53011b8df5c41f1eafd39e83a62d8
sarandrade <sarandradedias@gmail.com>
2020-11-26 20:03:18 -0300

    Alterações: INT0, INT1, atualizaDisplay

--- Projeto Final/Projeto_Final/Projeto_Final/main.c
+++ Projeto Final/Projeto_Final/Projeto_Final/main.c
@@ -1,20 +1,38 @@
 #include <atmel_start.h>
 
 // Variáveis Globais
-int8_t seg, min, hrs;
+int8_t seg, min, hrs, pausa;
 int16_t mili;
 char tarefas, tarefa_atual;
 
 // Interrupção externa 0, captura o Ligar/Desligar
 ISR(INT0_vect)
 {
-	
+	if(pausa == 1)
+	{
+		atualizaDisplay('l');
+		pausa = 0;
+	}
+	else
+	{
+		atualizaDisplay('d');
+		pausa = 1;
+	}
 }
 
 // Interrupção externa 1, captura o Play/Pause
 ISR(INT1_vect)
 {
-	
+	if(pausa == 1)
+	{
+		atualizaDisplay(tarefa_atual);
+		pausa = 0;
+	}
+	else
+	{
+		atualizaDisplay('p');
+		pausa = 1;
+	}
 }
 
 // Interrupção 0 por mudança de pino, captura o Finalizar Tarefa
@@ -38,84 +56,87 @@ ISR(PCINT0_vect)
 // Interrupção do TC0 a cada 1ms = (64*(249+1))/16MHz
 ISR(TIMER0_COMPA_vect)
 {
-	mili ++;
-	
-	if (hrs == 0)
+	if (pausa != 1)
 	{
-		S0 = S1 = S2 = 0;
-	}
-	
-	if (mili == 1000)
-	{
-		mili = 0;
-		seg ++;
+		mili ++;
 		
-		if (seg == 60)
+		if (hrs == 0)
 		{
-			seg = 0;
-			min ++;
+			S0 = S1 = S2 = 0;
+		}
+		
+		if (mili == 1000)
+		{
+			mili = 0;
+			seg ++;
 			
-			if (min == 5)
+			if (seg == 60)
 			{
-				OCR0A = 0.05*256;
-			}
-			else if (min == 15)
-			{
-				OCR0A = 0.25*256;
-			}
-			else if (min == 30)
-			{
-				OCR0A = 0.50*256;
-			}
-			else if (min ==45)
-			{
-				OCR0A = 0.75*256;
-			}
-			else if (min ==60)
-			{
-				OCR0A = 0.98*256;
-				min = 0;
-				hrs ++;
+				seg = 0;
+				min ++;
 				
-				if (hrs == 1)
+				if (min == 5)
 				{
-					S0 = 1;
-					S1 = S2 = 0;
-					led1 = 1;
+					OCR0A = 0.05*256;
 				}
-				if (hrs == 2)
+				else if (min == 15)
 				{
-					S1 = 1;
-					S0 = S2 = 0;
-					led2 = 1;
+					OCR0A = 0.25*256;
 				}
-				if (hrs == 3)
+				else if (min == 30)
 				{
-					S2 = 0;
-					S0 = S1 = 1;
-					led3 = 1;
+					OCR0A = 0.50*256;
 				}
-				if (hrs == 4)
+				else if (min ==45)
 				{
-					S2 = 1;
-					S0 = S1 = 0;
-					led4 = 1;
+					OCR0A = 0.75*256;
 				}
-				if (hrs == 5)
+				else if (min ==60)
 				{
-					S1 = 0;
-					S0 = S2 = 1;
-					led5 = 1;
-				}
-				if (hrs == 6)
-				{
-					S0 = 0;
-					S1 = S2 = 1;
-					led6 = 1;
+					OCR0A = 0.98*256;
+					min = 0;
+					hrs ++;
 					
-					// Finalizar
+					if (hrs == 1)
+					{
+						S0 = 1;
+						S1 = S2 = 0;
+						led1 = 1;
+					}
+					if (hrs == 2)
+					{
+						S1 = 1;
+						S0 = S2 = 0;
+						led2 = 1;
+					}
+					if (hrs == 3)
+					{
+						S2 = 0;
+						S0 = S1 = 1;
+						led3 = 1;
+					}
+					if (hrs == 4)
+					{
+						S2 = 1;
+						S0 = S1 = 0;
+						led4 = 1;
+					}
+					if (hrs == 5)
+					{
+						S1 = 0;
+						S0 = S2 = 1;
+						led5 = 1;
+					}
+					if (hrs == 6)
+					{
+						S0 = 0;
+						S1 = S2 = 1;
+						led6 = 1;
+						
+						// Finalizar
+					}
 				}
-			} 
+			}
 		}
 	}
 }
@@ -152,6 +173,7 @@ int main(void)-
 	seg = 0;
 	min = 0;
 	hrs = 0;
+	pausa = 1;
 	
 	nokia_lcd_init();
 	nokia_lcd_clear();
@@ -173,7 +195,46 @@ int main(void)-
 // Função para atualização do display PCD8544-7
 void atualizaDisplay(char entrada){
 	
-	if (entrada == 'f')
+	if (entrada == 'l')
+	{
+		nokia_lcd_clear();
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_set_cursor(0, 20);
+		nokia_lcd_write_string("Bem-vindo!!", 1);
+		nokia_lcd_set_cursor(0, 30);
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_render();
+		
+		_delay_ms(1000);
+		
+		nokia_lcd_clear();
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_set_cursor(0, 10);
+		nokia_lcd_write_string("Atividade", 1);
+		nokia_lcd_set_cursor(0, 20);
+		nokia_lcd_write_string(tarefa_atual, 1);
+		nokia_lcd_set_cursor(0, 30);
+		nokia_lcd_write_string("Em Andamento...", 1);
+		nokia_lcd_set_cursor(0, 40);
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_render();
+	}
+	else if (entrada == 'd')
+	{
+		nokia_lcd_clear();
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_set_cursor(0, 20);
+		nokia_lcd_write_string("Desligando...", 1);
+		nokia_lcd_set_cursor(0, 40);
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_render();
+		
+		_delay_ms(1000);
+		
+		nokia_lcd_clear();
+		
+	}
+	else if (entrada == 'f')
 	{
 		nokia_lcd_clear();
 		nokia_lcd_write_string("--------------", 1);
@@ -186,6 +247,17 @@ void atualizaDisplay(char entrada){
 		nokia_lcd_set_cursor(0, 40);
 		nokia_lcd_write_string("--------------", 1);
 		nokia_lcd_render();
+	}
+	else if (entrada == 'p')
+	{
+		nokia_lcd_clear();
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_set_cursor(0, 20);
+		nokia_lcd_write_string("Atividade Pausada", 1);
+		nokia_lcd_set_cursor(0, 40);
+		nokia_lcd_write_string("--------------", 1);
+		nokia_lcd_render();
+		
 	}
 	else if (entrada == 'x')
 	{
